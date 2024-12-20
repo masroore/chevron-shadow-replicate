bfd2704ded8b7a3fe592776b0ccf7293a8392853
Masroor Ehsan <masroore@gmail.com>
2024-12-20 10:21:40 +0600

    FIX - dest

--- cron.py
+++ cron.py
@@ -4,6 +4,7 @@ import arrow
 import yaml
 
 from src import db, dal
+from src.db import Database
 from src.models import LabOrder
 from src.utils import croak
 from src.catalogs import OrderContext
@@ -31,23 +32,31 @@ def purge_orders(dt: date):
             dal.purge_order_chain(ord.InvoiceId, db_)
 
 
-def ensure_user_shift(orders: list[LabOrder], dt: date, db_: db.Database) -> list[int]:
-    shifts = []
-    for ord in orders:
-        shift_id = dal.find_shift(ord.OrderingUserId, dt, db_)
+def recreate_shifts(orders: list[OrderContext], dt: date, db_: Database) -> list[int]:
+    user_ids = set([x.order.OrderingUserId for x in orders])
+    shift_map = {}
+
+    for uid in user_ids:
+        shift_id = dal.find_shift(uid, dt, db_)
         if not shift_id:
-            shift_id = dal.create_shift(ord.OrderingUserId, dt, db_)
+            shift_id = dal.create_shift(uid, dt, db_)
+        if shift_id:
+            shift_map[uid] = shift_id
+
+    shifts: list[int] = []
+    for ord in orders:
+        shift_id = shift_map.get(ord.order.OrderingUserId)
 
         if shift_id:
             db_.execute(
                 "UPDATE PROE.PatientLabOrders SET WorkShiftId = ? WHERE InvoiceId = ?",
                 shift_id,
-                ord.InvoiceId,
+                ord.order.InvoiceId,
             )
             db_.execute(
                 "UPDATE Finances.InvoiceTransactions SET WorkShiftId = ? WHERE InvoiceId = ?",
                 shift_id,
-                ord.InvoiceId,
+                ord.order.InvoiceId,
             )
             shifts.append(shift_id)
 
@@ -59,8 +68,6 @@ def src_scan_orders(dt: date, max_net: int) -> list[OrderContext]:
     with db.Database.make(DB_SRC) as db_:
         test_cat = dal.get_test_catalog(db_)
         invoices = dal.fetch_invoices(dt, db_)
-        shifts = ensure_user_shift(invoices, dt, db_)
-
         croak(f"Found {len(invoices)} invoices for {dt} | HWM: {max_net:,.0f}")
 
         contexts = []
@@ -80,32 +87,26 @@ def src_scan_orders(dt: date, max_net: int) -> list[OrderContext]:
             ctx.sanitize_tests(test_cat)
             contexts.append(ctx)
 
-        croak(f"Found {len(shifts)} shifts")
-        for sid in sorted(set(shifts)):
-            croak(f"Reconciling shift #{sid}")
-            dal.reconcile_shift(sid, True, db_)
-
     croak(f"Time-spreading {len(contexts)} orders")
     time_spread_invoices(contexts, dt)
 
     return contexts
 
 
-def dest_insert_chain(order: OrderContext):
+def dest_insert_chain(order: OrderContext, db_: Database):
     croak(f"INSERT #{order.order.InvoiceId} - {order.order.OrderId}")
-    with db.Database.make(DB_DEST) as db_:
-        if not dal.insert_order(order.order, db_):
-            return
+    if not dal.insert_order(order.order, db_):
+        return
 
-        invoice_id = dal.shadow_id_for_source_id(order.order.InvoiceId, db_)
-        croak(f"Src: {order.order.InvoiceId} -> Dest: {invoice_id}")
-        dal.insert_master(invoice_id, order.master, db_)
-        dal.insert_primal(invoice_id, order.primal, db_)
-        dal.insert_transactions(invoice_id, order.transactions, db_)
-        dal.insert_items(invoice_id, order.items, db_)
-        dal.insert_tests(invoice_id, order.tests, db_)
-        dal.insert_bundles(invoice_id, order.bundles, db_)
-        db_.commit()
+    invoice_id = dal.shadow_id_for_source_id(order.order.InvoiceId, db_)
+    croak(f"Src: {order.order.InvoiceId} -> Dest: {invoice_id}")
+    dal.insert_master(invoice_id, order.master, db_)
+    dal.insert_primal(invoice_id, order.primal, db_)
+    dal.insert_transactions(invoice_id, order.transactions, db_)
+    dal.insert_items(invoice_id, order.items, db_)
+    dal.insert_tests(invoice_id, order.tests, db_)
+    dal.insert_bundles(invoice_id, order.bundles, db_)
+    db_.commit()
 
 
 def filter_orders(orders: list[OrderContext], barrier: int) -> list[OrderContext]:
@@ -118,6 +119,14 @@ def filter_orders(orders: list[OrderContext], barrier: int) -> list[OrderContext
         result.append(o)
     croak(f"Filtered {len(result)} orders | HWM: {barrier} | Actual: {total}")
     return result
+
+
+def reconcile(shifts: list[int]):
+    croak(f"Found {len(shifts)} shifts")
+    with db.Database.make(DB_DEST) as db_:
+        for sid in sorted(set(shifts)):
+            croak(f"Reconciling shift #{sid}")
+            dal.reconcile_shift(sid, True, db_)
 
 
 def time_spread_invoices(orders: list[OrderContext], dt: date):
@@ -134,11 +143,17 @@ def time_spread_invoices(orders: list[OrderContext], dt: date):
         ord.order.OrderDateTime = sod + timedelta(seconds=i * interval)
 
 
+def populate_shadow(orders: list[OrderContext], dt: date, barrier: int):
+    with db.Database.make(DB_DEST) as db_:
+        recreate_shifts(orders, dt, db_)
+        for ord in orders:
+            dest_insert_chain(ord)
+
+
 if __name__ == "__main__":
     dt = arrow.now().date()
     purge_orders(dt)
     barrier = get_rand_hwm()
     orders = src_scan_orders(dt, barrier)
     # orders = filter_orders(orders, barrier)
-    for o in orders:
-        dest_insert_chain(o)
+    populate_shadow(orders, dt, barrier)
